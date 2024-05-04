991dbb76a4b62eb6437525244202338024b3c55c
hiengyen <ngyentrnghie777@gmail.com>
2024-05-04 17:49:29 +0700

    remove lang.go plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,7 +22,6 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,9 @@
 return {
   {
     "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+
     opt = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "stylua",
